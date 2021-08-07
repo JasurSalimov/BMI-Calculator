d0ac042a771fa6531bc9c4edb3feef86ad511923
Jasur Salimov <jasurtsalimov@gmail.com>
2021-08-08 00:36:50 +0500

    bmi calculatin impementation

--- BMI Calculator/Controller/ViewController.swift
+++ BMI Calculator/Controller/ViewController.swift
@@ -15,16 +15,18 @@ class ViewController: UIViewController {
     @IBAction func heightSlider(_ sender: UISlider) {
         heightLabel.text = "\(String(round(100*sender.value)/100)) m "
         bmiBrain.heightInit(h: Double(round(100*sender.value)/100))
+        print(bmiBrain.height)
     }
     @IBAction func weightSlider(_ sender: UISlider) {
         weightLabel.text = "\(String(round(10*sender.value)/10)) kg"
         bmiBrain.weightInit(w: Double(round(10*sender.value)/10))
+        print(bmiBrain.weight)
         
         
     }
     @IBAction func calculatePressed(_ sender: UIButton) {
         print("Button Calculate was pressed")
-        
+        print("BMI = \(bmiBrain.calculate())")
         
         
     }
--- BMI Calculator/Model/BmiBrain.swift
+++ BMI Calculator/Model/BmiBrain.swift
@@ -10,7 +10,7 @@ struct BmiBrain{
     
     var height: Double = 0
     var weight: Double = 0
-    
+    var bmi: Double = 0
     
    mutating func heightInit(h: Double){
         self.height = h
@@ -20,6 +20,13 @@ struct BmiBrain{
     mutating func weightInit(w: Double){
         self.weight = w
         
+        
+    }
+   mutating func calculate() -> String{
+        
+        self.bmi = weight/(height*height)
+        
+        return String((round(100*bmi)/100))
         
     }
 }
